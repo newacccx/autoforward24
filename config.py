c10b154148d275852ec4ccefaaf343f493fa64d4
newacccx <119074899+newacccx@users.noreply.github.com>
2024-02-24 23:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = int(environ.get("API_ID", "6381607"))
+    API_HASH = environ.get("API_HASH", "9799ad1623afe9bad664501f984b71fe")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5123698683:AAHOhqg_k9tx48LNGk7B4JdaOJD3nkKLM-g") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://newuser:newuser@cluster0.80t5h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "telegram")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1258695344').split()]
     
 class temp(object): 
     lock = {}
